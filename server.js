52cfad2e8452d3e4f250d7ace0a556b3480367c5
mohit <mohit.dhiman.sarkaghat@gmail.com>
2020-06-23 17:35:47 +0530

    Add to get request

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const fs = require("fs");
+const path = require("path");
+
+//GET ALL
+function getBlogs(req, res) {}  // I couldn't find the right way to list all the blogs
+// I tried readdir etc. but didn't work:(
+
+//GET ONE
+function getOneBlog(req, res) {
+  // How to get the tilte from the url parameters?
+  const title = req.params.title;
+  if (fs.existsSync(title)) {
+    res.sendFile(path.join(__dirname, title));
+  } else {
+    res.statusCode = 404;
+    res.end("Blog Post Does Not Exist");
+  }
+}
